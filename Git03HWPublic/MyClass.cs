9c773ab2de386c3ea19d71e4f271a5ab26f245ac
cbb356 <cbb356@gmail.com>
2025-06-05 00:31:00 +0300

    Solution added

---
+++ Git03HWPublic/MyClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Git03HWPublic
+{
+    internal class MyClass
+    {
+        public MyClass() 
+        {
+            Name = "MyClass";
+        }
+        public string Name { get; set; }
+    }
+}
---
+++ Git03HWPublic/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Git03HWPublic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Hello, {new MyClass().Name}!");
+        }
+    }
+}
